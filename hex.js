144596cf0bede61fab5adc7d0de08896f4e4dbfd
maxime <maxime.devos59@hotmail.fr>
2022-10-19 11:04:02 +0200

    colorswap optimisé

--- hex.js
+++ hex.js
@@ -2,17 +2,8 @@ const btn = document.querySelector(".btn-hero");
 const main = document.querySelector("body");
 const color = document.querySelector(".color");
 
-function generateRandomColor() {
-  let maxVal = 0xffffff;
-  let randomNumber = Math.random() * maxVal;
-  randomNumber = Math.floor(randomNumber);
-  randomNumber = randomNumber.toString(16);
-  let randColor = randomNumber.padStart(6, 0);
-  return `#${randColor.toUpperCase()}`;
-}
-
 btn.addEventListener("click", function () {
-  const couleurRandom = generateRandomColor();
-  main.style.backgroundColor = couleurRandom;
-  color.textContent = couleurRandom;
+  const hexColor = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
 });
